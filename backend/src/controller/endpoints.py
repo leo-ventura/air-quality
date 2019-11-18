fd023f5e9acd1396ade72b8491f0ff5cb2541a2f
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-18 01:34:23 -0300

    Endpoints now fetch information from the database

--- backend/src/controller/endpoints.py
+++ backend/src/controller/endpoints.py
@@ -7,19 +7,24 @@ from flask import Blueprint
 from flask import request
 
 from src.util.logger import get_logger
+from src.util.mysqlConnector import get_session
+
+from src.model.Estacao import Estacao
+from src.model.Analise import Analise
 
 logger = get_logger(sys.argv[0])
 
 blueprint = Blueprint("endpoints", __name__)
 
-@blueprint.route("/entidades", methods=['GET'])
-def get_information():
-    logger.info("Buscando informações de entidade")
-    data = {
-        "tabela": "Nome da Tabela",
-        "colunas": {
-            "codNum": 2,
-            "estacao": "BG"
-        }
-    }
+@blueprint.route("/estacoes", methods=['GET'])
+def get_estacoes():
+    logger.info("Buscando informações de sobre as estações")
+
+    session = get_session()
+
+    data = session.query(Estacao).all()
+    data = [d.format() for d in data]
+
+    session.close()
+
     return json.dumps(data)
