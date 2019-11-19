1a4673755ed6af165bb8edbfffe4d37a20e80a39
Leonardo Ventura <leonardo.venturacl@gmail.com>
2019-11-19 19:47:12 -0300

    Added new endpoints

--- backend/src/controller/endpoints.py
+++ backend/src/controller/endpoints.py
@@ -11,6 +11,7 @@ from src.util.mysqlConnector import get_session
 
 from src.model.Estacao import Estacao
 from src.model.Analise import Analise
+from src.model.QualidadeDoAr import QualidadeDoAr
 
 logger = get_logger(sys.argv[0])
 
@@ -22,7 +23,10 @@ def get_endpoints():
         "/estacoes": "Retorna todas as estacoes disponiveis",
         "/estacao": "Recebe filtros via parametro e retorna a estacao correspondente",
         "/analises": "Retorna todas as analises disponiveis",
-        "/analise": "Recebe filtros via parametro e retorna a(s) analise(s) correspondente(s)"
+        "/analise": "Recebe filtros via parametro e retorna a(s) analise(s) correspondente(s)",
+        "/todasQualidadeDoAr": "Retorna todas as informacoes sobre as qualidades do ar relativas a poluentes",
+        "/qualidadeDoAr": "Recebe filtros e retorna informacoes sobre a(s) qualidade(s) do ar relativas ao filtro aplicado",
+        "OBS": "Para todos endpoints que recebem filtro, o mesmo segue o padrao do nome da coluna no banco de dados. Exemplo: /analise?CO=0.6. Para colunas que o nome seja uma palavra, comece com letra minúscula. Exemplo: /qualidadeDoAr?siglaLocalEstacao=AV"
     }
 
     return json.dumps(data)
@@ -43,12 +47,13 @@ def get_estacoes():
 
 @blueprint.route("/estacao", methods=['GET'])
 def get_estacao():
+    logger.info(f'Buscando informações de sobre estacao')
+
     codigoEstacao = request.args.get("codigo")
     siglaLocal    = request.args.get("siglaLocal")
     latitude      = request.args.get("latitude")
     longitude     = request.args.get("longitude")
 
-    logger.info(f'Buscando informações de sobre estacao')
     logger.debug(f'Filtros: {codigoEstacao} {siglaLocal} {latitude} {longitude}')
 
     session = get_session()
@@ -88,6 +93,8 @@ def get_analises():
 
 @blueprint.route("/analise", methods=['GET'])
 def get_analise():
+    logger.info(f'Buscando informações de sobre um grupo de análises')
+
     analise_id          = request.args.get("id")
     CO                  = request.args.get("CO")
     CH4                 = request.args.get("CH4")
@@ -96,16 +103,14 @@ def get_analise():
     NOx                 = request.args.get("NOx")
     PM_10               = request.args.get("PM_10")
     PM_2_5              = request.args.get("PM_2_5")
-    temperatura         = request.args.get("Temperatura")
-    chuva               = request.args.get("Chuva")
-    pressao             = request.args.get("Pressao")
-    radiacaoSolar       = request.args.get("RadiacaoSolar")
-    umidadeRelativaDoAr = request.args.get("UmidadeRelativaDoAr")
-    direcaoVento        = request.args.get("DirecaoVento")
-    velocidadeVento     = request.args.get("VelocidadeVento")
-    estacaoCodigo       = request.args.get("EstacaoCodigo")
-    
-    logger.info(f'Buscando informações de sobre um grupo de análises')
+    temperatura         = request.args.get("temperatura")
+    chuva               = request.args.get("chuva")
+    pressao             = request.args.get("pressao")
+    radiacaoSolar       = request.args.get("radiacaoSolar")
+    umidadeRelativaDoAr = request.args.get("umidadeRelativaDoAr")
+    direcaoVento        = request.args.get("direcaoVento")
+    velocidadeVento     = request.args.get("velocidadeVento")
+    estacaoCodigo       = request.args.get("estacaoCodigo")    
 
     session = get_session()
 
@@ -145,6 +150,54 @@ def get_analise():
         query = query.filter(Analise.EstacaoCodigo == estacaoCodigo)
 
     logger.debug(f'Query: {str(query)}')
+
+    data = query.all()
+    data = [d.format() for d in data]
+
+    session.close()
+
+    return json.dumps(data)
+
+@blueprint.route("/todasQualidadeDoAr", methods=['GET'])
+def get_todas_qualidade_do_ar():
+    logger.info("Buscando informações sobre qualidade do ar")
+
+    session = get_session()
+
+    data = session.query(QualidadeDoAr).all()
+    data = [d.format() for d in data]
+
+    session.close()
+
+    return json.dumps(data)
+
+@blueprint.route("/qualidadeDoAr", methods=['GET'])
+def get_qualidade_do_ar():
+    logger.info("Buscando informações sobre qualidade do ar filtradas por colunas")
+
+    ID = request.args.get("id")
+    IQAR = request.args.get("iqar")
+    Data = request.args.get("data")
+    Poluente = request.args.get("poluente")
+    Classificacao = request.args.get("classificacao")
+    SiglaLocalEstacao = request.args.get("siglaLocalEstacao")
+
+    session = get_session()
+
+    query = session.query(QualidadeDoAr)
+
+    if ID is not None:
+        query = query.filter(QualidadeDoAr.ID == ID)
+    if IQAR is not None:
+        query = query.filter(QualidadeDoAr.IQAR == IQAR)
+    if Data is not None:
+        query = query.filter(QualidadeDoAr.Data == Data)
+    if Poluente is not None:
+        query = query.filter(QualidadeDoAr.Poluente == Poluente)
+    if Classificacao is not None:
+        query = query.filter(QualidadeDoAr.Classificacao == Classificacao)
+    if SiglaLocalEstacao is not None:
+        query = query.filter(QualidadeDoAr.SiglaLocalEstacao == SiglaLocalEstacao)
 
     data = query.all()
     data = [d.format() for d in data]
