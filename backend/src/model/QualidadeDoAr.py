086e1e2ed1a7eb3ebd2a319d4ae781a75cf3b4d8
Leonardo Ventura <leonardo.venturacl@gmail.com>
2019-11-19 19:29:40 -0300

    Added new table mapping

---
+++ backend/src/model/QualidadeDoAr.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, String, Integer, Date, Numeric, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from src.model.base import Base
+import json
+
+class QualidadeDoAr(Base):
+    __tablename__     = 'QualidadeDoAr'
+    ID                = Column(Integer, primary_key=True)
+    IQAR              = Column(Integer)
+    Data              = Column(Date)
+    Poluente          = Column(String)
+    Classificacao     = Column(String)
+    SiglaLocalEstacao = Column(String, ForeignKey('Estacao.SiglaLocal'))
+
+    def format(self):
+        return {
+            "ID": self.ID,
+            "IQAR": self.IQAR,
+            "Data": self.Data,
+            "Poluente": self.Poluente,
+            "Classificacao": self.Classificacao,
+            "SiglaLocalEstacao": self.SiglaLocalEstacao
+        }
