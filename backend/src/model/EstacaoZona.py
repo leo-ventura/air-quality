328861b4c6d15c39407f277e581c9916f92390e3
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-22 18:46:09 -0300

    Creating EstacaoZona table

---
+++ backend/src/model/EstacaoZona.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Table, Column, ForeignKey, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from src.model.base import Base
+import json
+
+EstacaoZona = Table('EstacaoZona', Base.metadata,
+    Column('EstacaoCodigo', Integer, ForeignKey('Estacao.Codigo')),
+    Column('Zona_id', Integer, ForeignKey('Zona.Zona_id'))
+)
