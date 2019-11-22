aff32d8ebee9e778940579446b65bb36e6962805
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-22 18:44:46 -0300

    Adding EstacaoZona to Zona

--- backend/src/model/Zona.py
+++ backend/src/model/Zona.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column, String, Integer, Date, Numeric
+from sqlalchemy import Column, String, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from src.model.base import Base
+from src.model.EstacaoZona import EstacaoZona
 import json
 
 class Zona(Base):
     __tablename__ = 'Zona'
-    Zona_id   = Column(Integer)
+    Zona_id   = Column(Integer, primary_key=True)
     Nome      = Column(String)
     Raio      = Column(Float)
     Latitude  = Column(Float)
     Longitude = Column(Float)
-
+    Estacoes  = relationship('Estacao', secondary=EstacaoZona)
 
     def format(self):
         return {
             "Zona_id": f'{self.Zona_id}',
             "Nome": self.Nome,
             "Raio": f'{self.Raio}',
-            "Latitude": f'{self.Latitude}'
+            "Latitude": f'{self.Latitude}',
             "Longitude": f'{self.Longitude}'
         }
