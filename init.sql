5699a7d3a507f6af94726a539028be1745a13468
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-22 16:46:02 -0300

    Updating DDL

--- init.sql
+++ init.sql
@@ -1,64 +1,77 @@
-CREATE TABLE Estacao (
-  Codigo VARCHAR PRIMARY KEY,
-  Bairro VARCHAR,
-  Latitude DOUBLE,
-  Longitude DOUBLE
-);
+CREATE TABLE `Estacao` (
+  `Codigo` int(11) NOT NULL,
+  `SiglaLocal` varchar(100) DEFAULT NULL,
+  `Latitude` decimal(15,5) DEFAULT NULL,
+  `Longitude` decimal(15,5) DEFAULT NULL,
+  `NomeEstacao` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Codigo`),
+  UNIQUE KEY `Estacao_UN` (`SiglaLocal`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-CREATE TABLE Analise (
-  DataHora DATE PRIMARY KEY,
-  fk_Estacao_Codigo VARCHAR,
-  C_O FLOAT,
-  N_O FLOAT,
-  N_O2 FLOAT,
-  N_Ox FLOAT,
-  PM_10 FLOAT,
-  PM_2_5 FLOAT,
-  GrauC FLOAT,
-  Chuva FLOAT,
-  Pressao FLOAT,
-  Radiacao_Solar FLOAT,
-  Umidade_relativa FLOAT,
-  Direcao FLOAT,
-  Velocidade FLOAT
-);
+CREATE TABLE `Analise` (
+  `Analise_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Data_e_hora` datetime NOT NULL,
+  `CO` decimal(15,5) DEFAULT NULL,
+  `CH4` decimal(15,5) DEFAULT NULL,
+  `NO` decimal(15,5) DEFAULT NULL,
+  `NO2` decimal(15,5) DEFAULT NULL,
+  `NOx` decimal(15,5) DEFAULT NULL,
+  `PM_10` decimal(15,5) DEFAULT NULL,
+  `PM_2_5` decimal(15,5) DEFAULT NULL,
+  `Temperatura` decimal(15,5) DEFAULT NULL,
+  `Chuva` decimal(15,5) DEFAULT NULL,
+  `Pressao` decimal(15,5) DEFAULT NULL,
+  `RadiacaoSolar` decimal(15,5) DEFAULT NULL,
+  `UmidadeRelativaDoAr` decimal(15,5) DEFAULT NULL,
+  `DirecaoVento` decimal(15,5) DEFAULT NULL,
+  `VelocidadeVento` decimal(15,5) DEFAULT NULL,
+  `EstacaoCodigo` int(11) NOT NULL,
+  PRIMARY KEY (`Analise_id`),
+  KEY `Analise_FK` (`EstacaoCodigo`),
+  CONSTRAINT `Analise_FK` FOREIGN KEY (`EstacaoCodigo`) REFERENCES `Estacao` (`Codigo`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=516976 DEFAULT CHARSET=latin1
 
-CREATE TABLE Zona (
-  Nome VARCHAR,
-  fk_Tags_Tags_PK INT,
-  ID INT PRIMARY KEY,
-  Raio FLOAT,
-  Latitude FLOAT,
-  Longitude FLOAT
-);
+CREATE TABLE `QualidadeDoAr` (
+  `ID` int(11) NOT NULL,
+  `IQAR` int(10) unsigned DEFAULT NULL,
+  `Data` datetime DEFAULT NULL,
+  `Poluente` varchar(100) DEFAULT NULL,
+  `Classificacao` varchar(100) DEFAULT NULL,
+  `SiglaLocalEstacao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `QualidadeDoAr_FK` (`SiglaLocalEstacao`),
+  CONSTRAINT `QualidadeDoAr_FK` FOREIGN KEY (`SiglaLocalEstacao`) REFERENCES `Estacao` (`SiglaLocal`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-CREATE TABLE Tags (
-  Tags_PK INT NOT NULL PRIMARY KEY,
-  Tags VARCHAR
-);
+CREATE TABLE `Zona` (
+  `Zona_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(100) DEFAULT NULL,
+  `Latitude` decimal(15,10) DEFAULT NULL,
+  `Longitude` decimal(15,10) DEFAULT NULL,
+  `Raio` decimal(15,10) DEFAULT NULL,
+  PRIMARY KEY (`Zona_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1
 
-CREATE TABLE pertence (
-  fk_Estacao_Codigo VARCHAR,
-  fk_Zona_ID INT
-);
+CREATE TABLE `Tag` (
+  `Tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `Tag` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Tag_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1
 
-ALTER TABLE Analise ADD CONSTRAINT FK_Analise_2
-  FOREIGN KEY (fk_Estacao_Codigo)
-  REFERENCES Estacao (Codigo)
-  ON DELETE CASCADE;
+CREATE TABLE `EstacaoZona` (
+  `EstacaoCodigo` int(11) NOT NULL,
+  `Zona_id` int(11) NOT NULL,
+  PRIMARY KEY (`EstacaoCodigo`,`Zona_id`),
+  KEY `EstacaoZona_FK_1` (`Zona_id`),
+  CONSTRAINT `EstacaoZona_FK` FOREIGN KEY (`EstacaoCodigo`) REFERENCES `Estacao` (`Codigo`) ON UPDATE CASCADE,
+  CONSTRAINT `EstacaoZona_FK_1` FOREIGN KEY (`Zona_id`) REFERENCES `Zona` (`Zona_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-ALTER TABLE Zona ADD CONSTRAINT FK_Zona_2
-  FOREIGN KEY (fk_Tags_Tags_PK)
-  REFERENCES Tags (Tags_PK)
-  ON DELETE NO ACTION;
-
-
-ALTER TABLE pertence ADD CONSTRAINT FK_pertence_1
-  FOREIGN KEY (fk_Estacao_Codigo)
-  REFERENCES Estacao (Codigo)
-  ON DELETE SET NULL;
-
-ALTER TABLE pertence ADD CONSTRAINT FK_pertence_2
-  FOREIGN KEY (fk_Zona_ID)
-  REFERENCES Zona (ID)
-  ON DELETE SET NULL;
+CREATE TABLE `ZonaTag` (
+  `Zona_id` int(11) NOT NULL,
+  `Tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`Zona_id`,`Tag_id`),
+  KEY `ZonaTag_FK_1` (`Tag_id`),
+  CONSTRAINT `ZonaTag_FK` FOREIGN KEY (`Zona_id`) REFERENCES `Zona` (`Zona_id`) ON UPDATE CASCADE,
+  CONSTRAINT `ZonaTag_FK_1` FOREIGN KEY (`Tag_id`) REFERENCES `Tag` (`Tag_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
