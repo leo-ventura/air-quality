40e9326d80aeb4a4db9ea3bb550a8befb9546ac5
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-22 18:44:07 -0300

    Adding EstacaoZona to Estacao

--- backend/src/model/Estacao.py
+++ backend/src/model/Estacao.py
@@ -3,9 +3,15 @@ from sqlalchemy import Column, String, Integer, Date, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from src.model.base import Base
+from src.model.EstacaoZona import EstacaoZona
 import json
 
 class Estacao(Base):
+    """
+        Mapeamento da tabela de Estacao
+        Analise e QualidadeDoAr possui um relacionamento One to Many
+        Enquanto Zonas é Many to Many relacionado pela tabela de ligação EstacaoZona
+    """
     __tablename__ = 'Estacao'
     Codigo        = Column(Integer, primary_key=True)
     SiglaLocal    = Column(String)
@@ -14,6 +20,7 @@ class Estacao(Base):
     Longitude     = Column(Numeric)
     Analises      = relationship('Analise')
     QualidadeDoAr = relationship('QualidadeDoAr')
+    Zonas         = relationship('Zona', secondary=EstacaoZona)
 
     def format(self):
         return {
