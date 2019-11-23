842852c6c0fe924a267a386d0ddfc851f7d3fdb9
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-23 02:00:39 -0300

    Adding option to rebuild specific containers

--- rebuild.sh
+++ rebuild.sh
@@ -3,25 +3,33 @@
 check_error () {
     if [[ $? -ne 0 ]]; then
         echo "[!] $1"
-        exit
+        exit 1
     fi
 }
+
+flag=$1
 
 echo "[*] Stopping containers"
 docker-compose down
 check_error "Error while trying to stop containers"
 
-echo "[*] Rebuilding mysql"
-docker-compose build --no-cache mysql
-check_error "Error while trying to build mysql container"
+if [ $flag = "" ] || [ $flag = "mysql" ]; then
+    echo "[*] Rebuilding mysql"
+    docker-compose build --no-cache mysql
+    check_error "Error while trying to build mysql container"
+fi
 
-echo "[*] Rebuilding backend"
-docker-compose build --no-cache backend
-check_error "Error while trying to build backend container"
+if [ $flag = "" ] || [ $flag = "backend" ]; then
+    echo "[*] Rebuilding backend"
+    docker-compose build --no-cache backend
+    check_error "Error while trying to build backend container"
+fi
 
-echo "[*] Rebuilding frontend"
-docker-compose build --no-cache frontend
-check_error "Error while trying to build frontend container"
+if [ $flag = "" ] || [ $flag = "frontend" ]; then
+    echo "[*] Rebuilding frontend"
+    docker-compose build --no-cache frontend
+    check_error "Error while trying to build frontend container"
+fi
 
 echo "[**] Recreating network and linking them"
 docker-compose up -d
