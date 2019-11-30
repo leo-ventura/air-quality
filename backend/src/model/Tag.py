b9db69e8cd75ec75e4bd3d0736271ffff5da8818
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-11-30 14:22:24 -0300

    Add Tag mapping

---
+++ backend/src/model/Tag.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from src.model.base import Base
+import json
+
+class Tag(Base):
+    __tablename__ = 'Tag'
+    Tag_id  = Column(Integer, primary_key=True)
+    Tag     = Column(String)
+    Zona_id = Column(Integer, ForeignKey('Zona.Zona_id'))
+    Zona    = relationship('Zona')
+
+    def format(self):
+        return {
+            "Tag_id": f'{self.Tag_id}',
+            "Tag": self.Tag,
+            "Zona_id": f'{self.Zona_id}'
+        }
