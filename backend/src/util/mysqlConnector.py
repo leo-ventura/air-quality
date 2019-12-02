28676587bc0e550a49292bb4ee973bd4306c91bf
Leonardo Ventura <leonardo.ventura@ufrj.br>
2019-12-02 00:42:22 -0300

    Redefining database user and password

--- backend/src/util/mysqlConnector.py
+++ backend/src/util/mysqlConnector.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 logger = get_logger(sys.argv[0])
 
 logger.info("Criando conexão com o banco de dados")
-engine = create_engine('mysql://root:dhdqdarj20192bd@172.10.0.2:3306/AirQuality', echo=True)
+engine = create_engine('mysql://bd:bd20192@172.10.0.2:3306/AirQuality', echo=True)
 
 logger.info("Inicializando pool de conexões")
 Session = sessionmaker(bind=engine)
